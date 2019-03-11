1e88cf26a36546fe9d9f52f03c35b8b4656f0fbf
Michelle Beshears <meeshysu@gmail.com>
2019-03-10 19:22:29 -0500

    exercise is working and finished

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,23 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type in a word to see if it is a Palindrome.");
+            string palindromeString, rev;
+            palindromeString = Console.ReadLine();
+            char[] characterReverse = palindromeString.ToCharArray();
+
+            Array.Reverse(characterReverse);
+            rev = new string(characterReverse);
+
+            bool reversePal = palindromeString.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (reversePal == true)
+            {
+                Console.WriteLine("" + palindromeString + " is s Palindrome!");
+            } else
+            {
+                Console.WriteLine("" + palindromeString + " is not a Palindrome!");
+            }
+            Console.ReadLine();
         }
     }
 }
